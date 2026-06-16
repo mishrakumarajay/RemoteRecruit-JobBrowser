a3b3c31d987321f2eab479a7b908161cd1982c8c
Ajay Mishra <gaurav.patil0911@gmail.com>
2026-06-16 21:57:57 +0530

    chore: initial project setup with Clean Architecture skeleton

---
+++ RemoteRecruit/App/RemoteRecruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteRecruitApp.swift
+//  RemoteRecruit
+//
+//  Created by Ajay Mishra on 16/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct RemoteRecruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
